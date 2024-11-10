1fb972fcd45721eff5b1cc4d982f24ea7f85c02f
thaiha-hub <devops.engineer0528@gmail.com>
2024-11-10 14:41:58 +0200

    edit bash3

--- bash_scripts/bash3.sh
+++ bash_scripts/bash3.sh
@@ -1,1 +1,11 @@
 
+
+#!/usr/bin/env bash
+
+# Fråga användare om ett namn
+read -p "Ange ett namn: " name
+
+#Skapa en fil med  detta namn
+touch  ${name}.txt
+
+echo "Filen ${name}.txt har skapats"
